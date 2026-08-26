a1e303607e31c95fa13568c67557e50426847cdb
asthabhati076-cmyk <asthabhati076@gmail.com>
2026-08-26 10:18:49 +0530

    Add files via upload

---
+++ Matrix_transpose.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], trans[10][10], r, c, i, j;
+    
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+    
+    printf("Enter matrix elements:\n");
+    for (i = 0; i < r; ++i) {
+        for (j = 0; j < c; ++j) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    
+    printf("\nEntered Matrix:\n");
+    for (i = 0; i < r; ++i) {
+        for (j = 0; j < c; ++j) {
+            printf("%d  ", a[i][j]);
+        }
+        printf("\n");
+    }
+    
+    for (i = 0; i < r; ++i) {
+        for (j = 0; j < c; ++j) {
+            trans[j][i] = a[i][j];
+        }
+    }
+    
+    printf("\nTranspose of Matrix:\n");
+    for (i = 0; i < c; ++i) {
+        for (j = 0; j < r; ++j) {
+            printf("%d  ", trans[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
